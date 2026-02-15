af66654bd5f1836ba6eaf456c887ff87499e6c44
aasmaa01 <aasmaelearn@gmail.com>
2026-02-16 00:10:56 +0100

    Remove argument parsing and related functionality, simplifying main() to a welcome message

--- bin/library_management.dart
+++ bin/library_management.dart
@@ -1,57 +1,21 @@
-import 'package:args/args.dart';
+//enums 
 
-const String version = '0.0.1';
+//→ exceptions 
 
-ArgParser buildParser() {
-  return ArgParser()
-    ..addFlag(
-      'help',
-      abbr: 'h',
-      negatable: false,
-      help: 'Print this usage information.',
-    )
-    ..addFlag(
-      'verbose',
-      abbr: 'v',
-      negatable: false,
-      help: 'Show additional command output.',
-    )
-    ..addFlag('version', negatable: false, help: 'Print the tool version.');
-}
+//→ mixins 
 
-void printUsage(ArgParser argParser) {
-  print('Usage: dart library_management.dart <flags> [arguments]');
-  print(argParser.usage);
-}
+//→ abstract 
 
-void main(List<String> arguments) {
-  final ArgParser argParser = buildParser();
-  try {
-    final ArgResults results = argParser.parse(arguments);
-    bool verbose = false;
+//→ classes 
 
-    // Process the parsed arguments.
-    if (results.flag('help')) {
-      printUsage(argParser);
-      return;
-    }
-    if (results.flag('version')) {
-      print('library_management version: $version');
-      return;
-    }
-    if (results.flag('verbose')) {
-      verbose = true;
-    }
+//→ manager 
 
-    // Act on the arguments provided.
-    print('Positional arguments: ${results.rest}');
-    if (verbose) {
-      print('[VERBOSE] All arguments: ${results.arguments}');
-    }
-  } on FormatException catch (e) {
-    // Print usage information if an invalid argument was provided.
-    print(e.message);
-    print('');
-    printUsage(argParser);
-  }
+//→ isolate worker 
+
+//→ helpers 
+
+//→ main()
+
+void main(){
+  print("Welcome to the Library Management System!");
 }
