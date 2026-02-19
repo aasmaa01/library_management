c11c9611d778fe926bacb71b9eb9219124eb7209
aasmaa01 <aasmaelearn@gmail.com>
2026-02-19 20:37:28 +0100

    Refactor library management classes and exceptions; enhance Book and Member functionality

--- bin/library_management.dart
+++ bin/library_management.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:isolate';
+import 'dart:io';
 //we have local library.!
 //The librarian needs to manage books
 /*
@@ -20,78 +23,207 @@ Who converts objects to Map?
 //Handle reservations (books held for members)
 
 //enums ~ Book status (available, borrowed, reserved) ~ Member status (active, inactive) ~ Loan status (active, overdue, returned) ~ Reservation status (active, fulfilled, cancelled)
-enum BookStatus { available, loaned, reserved } //its life -> available -> loaned  -> available-> reserved 
 
-enum MemberTier { standard, premium } // 
+enum BookStatus {
+  //its life -> available -> loaned  -> available-> reserved
+  available, // on the shelf, anyone can borrow it
+  loaned, // currently borrowed by a member
+  reserved, // held for a specific premium member
+}
+
+enum MemberTier {
+  standard,
+  premium,
+} // premium can reserve books, standard cannot
 
 //→ exceptions ~ Custom errors
-class BookNotFoundExcp implements Exception { //loan,find, return a book id that doesnt exist in my map 
-  String message;
-  BookNotFoundExcp(this.message);
-}
-class AlreadyLoanedExcp implements Exception{
-  //when loan a book whose status already BookStatus.loaned
-  String message;
-  AlreadyLoanedExcp(this.message);
+class BookNotFoundExcp implements Exception {
+  //loan,find, return a book id that doesnt exist in my map
+  final String message;
+  const BookNotFoundExcp(this.message);
 
+  @override
+  String toString() => 'BookNotFoundExcp: $message';
+}
+
+class AlreadyLoanedExcp implements Exception {
+  //when loan a book whose status already BookStatus.loaned
+  final String message;
+  const AlreadyLoanedExcp(this.message);
+
+  @override
+  String toString() => 'AlreadyLoanedExcp: $message';
+}
+
+class MemberLimitExcp implements Exception {
+  final String message;
+  const MemberLimitExcp(this.message);
+
+  @override
+  String toString() => 'MemberLimitExcp: $message';
 }
 
 //→ mixins ~
-mixin Loggable{
-  void log(String message){
+mixin Loggable {
+  void log(String message) {
     //print a formated message with timestamp
-    print("[${DateTime.now()}] $message");
+    print("\u{1F4DD} [${DateTime.now()}] $message");
   }
 }
-mixin Timestamped{
-  DateTime createdAt = DateTime.now();
-  //auto set to now 
-  
+mixin Timestamped {
+  final DateTime createdAt = DateTime.now();
+  //auto set to now
 }
 
 //→ abstract
-abstract class LibraryItem{
-  int id;
-  String title;
+abstract class Item {
+  final String id;
+  final String title;
 
-  LibraryItem(this.id, this.title);
+  Item(this.id, this.title);
+  void displayInfo() {
+    print("ID: $id, Title: $title");
+  }
 
   //abstract method
-  String get details();
+  String get details;
 }
-abstract class Person{
-  int id;
+
+abstract class Person {
+  final String id;
   String name;
 
   Person(this.id, this.name);
 
   //abstract method
-  String get contactInfo();
+  String get contactInfo;
 }
 
-//→ classes Book, Member, Loan...
-class Book extends LibraryItem implements Comparable<Book> with Loggable, Timestamped {
-  String author;
+//searchable interface
+abstract class IsSearchable {
+  bool matches(String query);
+}
+
+//→ classes Book, Member, Loan
+class Book extends Item with Loggable, Timestamped implements IsSearchable {
+  final String author;
+  final Set<String>
+  genres; //crime, fiction, mystery, science, history, religion
   BookStatus status;
   String? description; //can be null
 
+  Book(
+    String id,
+    String title,
+    this.author, {
+    required this.genres,
+    this.status = BookStatus.available,
+    this.description,
+  }) : super(id, title);
+
+  Book.mystery(String id, String title, String author)
+    : author = author,
+      genres = {'Mystery'},
+      status = BookStatus.available,
+      description = null,
+      super(id, title);
+
+  @override
+  String get details =>
+      "Book \u{1F4D6} : $title by $author | Status: ${status.name.toUpperCase()} | Genres: ${genres.join(', ')} | ${description ?? 'No description'}";
+
+  @override
+  bool matches(String query) =>
+      title.toLowerCase().contains(query.toLowerCase()) ||
+      author.toLowerCase().contains(query.toLowerCase()); //search by title or author
+
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'title': title,
+    'author': author,
+    'genres': genres.toList(),
+    'status': status.name,
+    'description': description,
+  };
+
+  factory Book.fromMap(Map<String, dynamic> map) {
+    //convert a Map back to a Book object
+    return Book(
+      map['id'] as String,
+      map['title'] as String,
+      map['author'] as String,
+      genres: Set<String>.from(map['genres'] as List),
+      status: BookStatus.values.byName(map['status'] as String),
+      description: map['description'] as String?,
+    );
+  }
 }
 
 class Member extends Person with Loggable, Timestamped {
-  int id;
-  String name;
-  String email;
-  MemberTier tier; //standard or premium
+  final String email;
+  final MemberTier tier; //standard or premium
 
-  Member(int id, String title, this.name, this.email, this.tier) : super(id, title);
+  Member(String id, String name, this.email, this.tier) : super(id, name);
+
+  Member.premium(String id, String name, String email)
+    : email = email,
+      tier = MemberTier.premium,
+      super(id, name);
+
+  @override
+  String get contactInfo =>
+      "Member \u{1F464} : $name | Email: $email | Tier: ${tier.name.toUpperCase()}";
 }
 
-class Loan {
-  int id;
-  int bookId;
-  int memberId;
-  DateTime loanDate;
+class PremiumMember extends Member {
+  final List<String> reservationHistory = [];
+
+  PremiumMember(String id, String name, String email)
+    : super.premium(id, name, email);
+
+  @override
+  String get contactInfo => '${super.contactInfo}  Premium Member \u{1F451}';
+
+  void addReservation(String bookId) {
+    reservationHistory.add(bookId);
+    log("Added reservation for book ID: $bookId to member $name's history.");
+  }
+
+  void cancelReservation(String bookId) {
+    reservationHistory.remove(bookId);
+    log(
+      "Cancelled reservation for book ID: $bookId from member $name's history.",
+    );
+  }
+}
+
+class Loan with Timestamped {
+  final String id;
+  final int bookId;
+  final int memberId;
+  final DateTime loanDate;
   DateTime? returnDate; //can be null if not returned yet
+
+  Loan({
+    required this.id,
+    required this.bookId,
+    required this.memberId,
+    required this.loanDate,
+  });
+
+  Loan.returned({
+    required this.id,
+    required this.bookId,
+    required this.memberId,
+    required this.loanDate,
+    required this.returnDate,
+  });//named constructor for returned loans
+
+  bool get isReturned => returnDate != null;//check if the loan has been returned
+  String get summary=>
+    'Loan[$id]: Book $bookId loaned to Member $memberId'
+    'Loaned: ${loanDate.toLocal().toString().substring(0, 10)} |'
+    'Returned: ${returnDate?.toLocal().toString().substring(0, 10) ?? "Not returned yet"}';
 }
 
 //→ manager  ~ handle all operations
@@ -101,14 +233,10 @@ class Loan {
 //→ helpers
 
 //→ main() async menu loop
-const version= '0.0.1';
- void printUsage(){
-  print("hejeje: 'help', 'version', 'search <ARTICLE-TITLE>'");
- }
+
 void main(List<String> arguments) {
   print('Welcome to the Library Management System! ');
-  List<String> books=['Murder on the orient Express', 'Death on the Nile','The zahir', 'الداء والدواء'];
-  //Map<String, dynamic> 
+  //Map<String, dynamic>
   //DATA — Books in Map, Loans in List, Members in Map
 
   //menu options : add/update/find/delete book, loans, members, reservations
